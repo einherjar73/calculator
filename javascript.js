8e494c0411cf43cda9b419d171a3f9c6d4b6cb0c
Dima Shubitidze <einherjar73@users.noreply.github.com>
2025-04-24 14:43:27 +0400

    Made visual Made html and css for calculator

---
+++ javascript.js
@@ -0,0 +1,32 @@
+const clear = document.querySelector("#clear");
+const positNegat = document.querySelector("#positive-negative");
+const percent = document.querySelector("#percent");
+const add = document.querySelector("#add");
+const one = document.querySelector("#one");
+const two = document.querySelector("#two");
+const three = document.querySelector("#three");
+const subtract = document.querySelector("#subtract");
+const four = document.querySelector("#four");
+const five = document.querySelector("#five");
+const six = document.querySelector("#six");
+const multiply = document.querySelector("#multiply");
+const seven = document.querySelector("#seven");
+const eight = document.querySelector("#eight");
+const nine = document.querySelector("#nine");
+const divide = document.querySelector("#divide");
+const zero = document.querySelector("#zero");
+const dot = document.querySelector("#dot");
+const equals = document.querySelector("equals");
+const display = document.querySelector("display");
+const digits = document.querySelector("digits");
+let numberA;
+let numberB;
+let operator;
+
+function operate(numberA, numberB, operator){
+
+}
+
+
+
+
