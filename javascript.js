0bb3929bcd0e94fadfa690f7732218da0555af4f
Dima Shubitidze <einherjar73@users.noreply.github.com>
2025-04-24 18:28:26 +0400

    added some functional added some functional shows digits on display

--- javascript.js
+++ javascript.js
@@ -16,17 +16,83 @@ const nine = document.querySelector("#nine");
 const divide = document.querySelector("#divide");
 const zero = document.querySelector("#zero");
 const dot = document.querySelector("#dot");
-const equals = document.querySelector("equals");
-const display = document.querySelector("display");
-const digits = document.querySelector("digits");
-let numberA;
-let numberB;
+const equals = document.querySelector("#equals");
+const display = document.querySelector("#display");
+const digits = document.querySelectorAll(".digit");
+const operators = document.querySelectorAll(".operator");
+let number;
 let operator;
+let dispNums = [];
+let newArr = [];
+let result;
+let operatorIsClicked = false;
 
-function operate(numberA, numberB, operator){
+for (let i = 0; i < 10; i++) {
+  digits[i].addEventListener("click", (e) => {
+    if (operatorIsClicked) {
+      display.textContent = "";
+      dispNums = [];
+    }
+    if (dispNums.length != 10) {
+      dispNums.push(e.target.textContent);
+    }
 
+    display.textContent = dispNums.join("");
+    number = parseFloat(display.textContent);
+    newArr.push(number);
+    operatorIsClicked = false;
+  });
 }
 
+for (let i = 0; i < 4; i++) {
+  operators[i].addEventListener("click", (e) => {
+    operator = e.target.textContent;
+    operatorIsClicked = true;
+  });
+}
 
+equals.addEventListener("click", () => {
+  operate;
+});
 
+function operate(numberA, numberB, operator) {
+  switch (operator) {
+    case "+":
+      return addNums(numberA, numberB);
 
+    case "-":
+      return subtractNums(numberA, numberB);
+
+    case "*":
+      return multiplyNums(numberA, numberB);
+
+    case "/":
+      return divideNums(numberA, numberB);
+
+    default:
+      return;
+  }
+}
+
+clear.addEventListener("click", () => {
+  dispNums = [];
+  newArr = [];
+  display.textContent = dispNums;
+  operatorIsClicked = false;
+});
+
+function addNums(a, b) {
+  return a + b;
+}
+
+function subtractNums(a, b) {
+  return a - b;
+}
+
+function multiplyNums(a, b) {
+  return a * b;
+}
+
+function divideNums(a, b) {
+  return a / b;
+}
